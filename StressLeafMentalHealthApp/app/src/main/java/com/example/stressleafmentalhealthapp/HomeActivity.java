bd89a7a0504ad1b4b375f9f979ad2c8166916cb1
richy6 <richardwei6@gmail.com>
2020-01-04 14:12:41 -0800

    Font Size Slider Implemented
    Need to figure out how to save the value

--- StressLeafMentalHealthApp/app/src/main/java/com/example/stressleafmentalhealthapp/HomeActivity.java
+++ StressLeafMentalHealthApp/app/src/main/java/com/example/stressleafmentalhealthapp/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.stressleafmentalhealthapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
 
@@ -9,15 +10,17 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-    float fontSize = 14; // default font size
+    int fontSize = 14;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,17 @@ public class HomeActivity extends AppCompatActivity {
         Button btn3 = (Button) findViewById(R.id.GoToArticle3_btn);
         Button btn4 = (Button) findViewById(R.id.GoToArticle4_btn);
         Button btn5 = (Button) findViewById(R.id.GoToArticle5_btn);
+
+        /*SharedPreferences getsp  = PreferenceManager.getDefaultSharedPreferences(this);
+        int savedfontsize = getsp.getInt("SavedFontSize",fontSize);
+        if (savedfontsize == fontSize){
+            SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putInt("SavedFontSize",fontSize);// saves fontSize
+            editor.apply();
+        }
+        fontSize = savedfontsize;*/
+        //TODO: fix saving the fontsize method ^
 
         /*androidx.constraintlayout.widget.ConstraintLayout layout = (androidx.constraintlayout.widget.ConstraintLayout) findViewById(R.id.HomeLayout);
         // TODO: ^ causes the app to crash - https://github.com/richardwei6/Mental-Health-Awareness-App/issues/1
@@ -40,6 +54,7 @@ public class HomeActivity extends AppCompatActivity {
 
             } // needs to be in a different function
         }*/
+
     }
     // TODO: when opening list view and articles on a real device, it causes 140+ skipped frames - needs optimization/reduce asset file sizes. https://stackoverflow.com/questions/14678593/the-application-may-be-doing-too-much-work-on-its-main-thread/21126690
     public void setHomeview(View view)
@@ -47,7 +62,35 @@ public class HomeActivity extends AppCompatActivity {
         setContentView((R.layout.activity_main));
     }
 
-    public void setSettingsview(View view) { setContentView((R.layout.settings_layout)); }
+    public void setSettingsview(View view) { setContentView((R.layout.settings_layout));
+        TextView fontTxt = (TextView) findViewById(R.id.fontSize);
+        fontTxt.setText(String.valueOf(fontSize));
+        final SeekBar fsk=(SeekBar) findViewById(R.id.fontSlider);
+        fsk.setProgress(fontSize);
+        fsk.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress!=0){
+                    fontSize = progress;
+                }
+                else{
+                    fontSize = 1;
+                }
+                TextView fontTxt = (TextView) findViewById(R.id.fontSize);
+                fontTxt.setText(String.valueOf(fontSize));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
 
     public void setListview(View view) { setContentView((R.layout.listview_layout)); }
 
@@ -75,6 +118,8 @@ public class HomeActivity extends AppCompatActivity {
     {
         setContentView((R.layout.article5_layout));
     }
+
+
 
    /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
